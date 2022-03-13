ea156e4ca9e42ef56bd8c08fe3632756f6e679e5
Gami13 <gamiofficial0@gmail.com>
2022-03-13 23:47:12 +0100

    zrobilem strone glowna (kinda) dziala na duzy ekran i telefon, po srodku sie pierdoli, teraz ide spac, naraska przegrywy

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <?php include_once('header.php');
 giveHead('Główna');
-?>
+?><h1 class="chuj2" style="position: relative; width: 100%; text-align:center; margin-top:1rem">Ignacy Łukasiewicz</h1>
 <div class="chuj">
 
-<section class="left">
-    <div  class="obrazek">
-        <img src="img/img1.jpg" alt="">
-        <span>Życiorys</span>
-    </div>
-    <div  class="obrazek">
-        <img src="img/img1.jpg" alt="">
-        <span>Quiz</span>
-    </div>
-</section>
-<section class="middle">
-<img src="img/img1.jpg">
-</section>
-<section class="right">
-<div  class="obrazek">
-        <img src="img/img1.jpg" alt="">
-        <span>Galeria Zdjęć</span>
-    </div>
-    <div  class="obrazek">
-        <img src="img/img1.jpg" alt="">
-        <span>Mapa</span>
-    </div>
-</section>
+    <section class="mainFragment" >
+        <div class="obrazek mainItem" onclick="window.location.pathname += '/life.php'">
+            <img src="img/img1.jpg" alt="">
+            <span>Życiorys</span>
+        </div>
+        <div class="obrazek mainItem" onclick="window.location.pathname += '/quizStart.php'">
+            <img src="img/img1.jpg" alt="">
+            <span>Quiz</span>
+        </div>
+    </section>
+    <section class="mainFragment">
+        <img class="mainImage" src="img/img1.jpg">
+    </section>
+    <section class="mainFragment">
+        <div class="obrazek mainItem" onclick="window.location.pathname += '/gallery.php'">
+            <img src="img/img1.jpg" alt="">
+            <span>Galeria</span>
+        </div>
+        <div class="obrazek mainItem" onclick="window.location.pathname += '/map.php'">
+            <img src="img/img1.jpg" alt="">
+            <span>Mapa</span>
+        </div>
+    </section>
 </div>
 
 <?php
