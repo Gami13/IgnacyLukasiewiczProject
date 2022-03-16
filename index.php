0a4c441e39e15b867ade773ad5fc3f13598f2cee
Gami13 <gamiofficial0@gmail.com>
2022-03-16 13:08:07 +0100

    final commit ig

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php include_once('header.php');
 giveHead('Główna');
 ?>
-<h1 class="chuj2" style="width: 100%; text-align:center; font-size:5rem; margin-bottom: -2rem; margin-top:1rem">Ignacy Łukasiewicz</h1>
-<div class="chuj">
 
+<div class="mainWrapper">
+<h1 style="width: 100%; text-align:center; font-size:5rem; margin-bottom: -3rem; margin-top:1rem">Ignacy Łukasiewicz</h1>
     <section class="mainFragment" >
         <div class="obrazek mainItem" onclick="window.location.pathname += '/life.php'">
-            <img src="img/img1.jpg" alt="">
+            <img src="https://www.odleglosci.pl/galeria-081324/Tablica-na-grobie-Honoraty-Lukasiewicz.jpg" alt="">
             <span>Życiorys</span>
         </div>
         <div class="obrazek mainItem" onclick="window.location.pathname += '/quizStart.php'">
-            <img src="img/img1.jpg" alt="">
+            <img src="https://citik.jaslo.pl/wp-content/uploads/2017/06/apteka_lukasiewicza-1.jpg" alt="">
             <span>Quiz</span>
         </div>
     </section>
@@ -21,7 +21,7 @@ giveHead('Główna');
             <span>Galeria</span>
         </div>
         <div class="obrazek mainItem" onclick="window.location.pathname += '/map.php'">
-            <img src="img/img1.jpg" alt="">
+            <img src="img/map.png" alt="">
             <span>Mapa</span>
         </div>
     </section>
